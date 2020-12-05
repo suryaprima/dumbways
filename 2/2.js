831964539c91b2ad6d092be961524912ff7b7f03
Surya Prima Siregar <sprima85@gmail.com>
2020-12-05 20:51:01 +0700

    update

--- 2/2.js
+++
@@ -1,9 +0,0 @@
-function putarArray(arr=[]) {
-    let arrTemp = ""
-    for(i=1; i < arr.length; i++){
-        arrTemp = arr.shift()
-        arr.push(arrTemp)
-        console.log("putaran "+i+": ["+arr+"]")
-    }
-}
-putarArray(["a","b","c","d","e"])
